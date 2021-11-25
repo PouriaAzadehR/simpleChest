f4168c8cfff3f8219891e4e7d4721849a4c8fe9e
PouriaAzadehR <pouriazadeh81@aut.ac.ir>
2021-11-25 13:29:32 +0330

    move method added

--- Actions.java
+++ Actions.java
@@ -1,4 +1,13 @@
 package com.company;
 
+/**
+ * this is an element which is used for actions of diffrent elements of game
+ * it contains signature of method MOVE
+ * @author pouri
+ * @version 1
+ * @since today
+ */
 public interface Actions {
+
+    void move(Board gameBoard);
 }
