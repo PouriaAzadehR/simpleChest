a08a11aea230b232f97fb44c38716a504db8b3a8
PouriaAzadehR <pouriazadeh81@aut.ac.ir>
2021-11-25 14:45:35 +0330

    move method overrided

--- King.java
+++ King.java
@@ -1,11 +1,30 @@
 package com.company;
 
+import java.util.Scanner;
+
 /**
  * this is a class for king element
  * @version 1
  * @author pouri
  * @since today
  */
-public class King {
+public class King extends Element implements Actions {
 
+    /**
+     * this is a constructor
+     * @param color of king
+     * @param coordination of king in board
+     */
+    public King(char color,int coordination){
+        super(color,coordination);
+    }
+
+    @Override
+    public void move(Board gameBoard) {
+        System.out.println("this is your king ");
+        System.out.println("please direction of your move");
+        Scanner scanner=new Scanner(System.in);
+        String direction =scanner.next();
+        moveBoard(gameBoard,1,direction.charAt(0));
+    }
 }
